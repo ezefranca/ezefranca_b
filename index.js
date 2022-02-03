df1f7fa5374ee79a68a70f20045e83457f0b0449
Ezequiel Santos <ezequiel.ifsp@gmail.com>
2022-02-02 22:12:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ async function generateReadMe() {
   .then(screen => {
     fs.writeFileSync(`img_history/${Date.now()}.png`, screen);
     fs.writeFileSync(`data_history/${Date.now()}.MD`, inner_html)
-    let finalReadMe = `[![README build](https://github.com/ezefranca/ezefranca/actions/workflows/main.yaml/badge.svg)](https://github.com/ezefranca/ezefranca/actions/workflows/main.yaml) <br> ${inner_html} <br> (This README is updated every 15 minutes) last update <code>${Date()}</code>` 
+    let finalReadMe = `## Hi, <br> ${inner_html} <br> (This README is updated every 15 minutes) last update <code>${Date()}</code>` 
     fs.writeFileSync('README.md', finalReadMe);
   })
   
