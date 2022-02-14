26ee34d0312b1f3d9bffeaeae6ff44b31bc63120
Ezequiel França <ezequiel.ifsp@gmail.com>
2022-02-14 10:35:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ async function generateReadMe() {
   .then(screen => {
     fs.writeFileSync(`img_history/${Date.now()}.png`, screen);
     fs.writeFileSync(`data_history/${Date.now()}.MD`, inner_html)
-    let finalReadMe = `<h2><img height="30px" width="30px" src="https://camo.githubusercontent.com/e8e7b06ecf583bc040eb60e44eb5b8e0ecc5421320a92929ce21522dbc34c891/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f6876524a434c467a6361737252346961377a2f67697068792e676966"></img>Hi, my name is Ezequiel</h2> <blockquote> I'm a software developer who enjoys building creative things. </blockquote> <ul> ${items}  </ul> <ul><li>:octocat: This README is updated every ~60 minutes </li><li> :shipit: last update <code>${Date()}</code></li></ul><br>` 
+    let finalReadMe = `<h2><img height="30px" width="30px" src="https://camo.githubusercontent.com/e8e7b06ecf583bc040eb60e44eb5b8e0ecc5421320a92929ce21522dbc34c891/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f6876524a434c467a6361737252346961377a2f67697068792e676966"></img>Hi, my name is Ezequiel</h2><hr style='background-color:#e67e22;border-width:0;color:#000000;height:8px;line-height:0;text-align:left;width:50%;'/> <blockquote> I'm a software developer who enjoys building creative things. </blockquote> <ul> ${items}  </ul> <ul><li>:octocat: This README is updated every ~60 minutes </li><li> :shipit: last update <code>${Date()}</code></li></ul><br>` 
     fs.writeFileSync('README.md', finalReadMe);
   })
   
