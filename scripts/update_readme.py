d2d638dfd0e2da12aaf6658ca4d9cec55925fde9
Ezequiel Santos <ezequiel.ifsp@gmail.com>
2024-04-29 23:42:36 +0100

    Update update_readme.py

--- scripts/update_readme.py
+++ scripts/update_readme.py
@@ -6,6 +6,21 @@ import os
 # Obtain API keys from environment variables
 LASTFM_API_KEY = os.getenv('LASTFM_API_KEY')
 
+def get_current_bio():
+    current_date = datetime.datetime.now()
+    day_name = current_date.strftime('%A')
+    date_str = current_date.strftime('%d of %B of %Y')
+
+    # Create a bio content with context and emojis
+    bio_content = (
+        f"👋 Hello, nice {day_name}, on {date_str}. I am working as a Mobile Developer "
+        f"🙋🏻‍♂️ I'm Ezequiel (Ezekiel), a developer and creative technologist. I enjoy creating software tools and making basil pesto.\n"
+        f"💼📱 [@miniclip](https://github.com/miniclip) and advancing my PhD studies in Technology 👨🏻‍💻🎓 [@iade-pt](https://github.com/iade-pt). "
+        f"👾 Passionate about game development and leveraging tech for creative solutions!"
+    )
+
+    return bio_content
+
 def get_last_posts(limit=3):
     rss_url = "http://ezefranca.com/feed.rss"
     feed = feedparser.parse(rss_url)
@@ -35,16 +50,22 @@ def get_last_song():
     else:
         return None
 
-def update_readme(posts, song):
+def update_readme(posts, song, bio):
     day_name = datetime.datetime.now().strftime('%A')
     with open('README.md', 'w') as file:
         # Writing the greeting with the day name
-        file.write(f"> Happy {day_name}! 👋🏻\n\n")
+        if bio:
+            file.write(f"{bio}\n")
 
         # Writing the tip section
         file.write("> [!TIP]\n")
-        file.write("> Hey, 👋🏻, I'm Ezequiel (Ezekiel), an iOS developer and Creative Technologist with experience in various areas of technology such as mobile software development, game development, project design, software engineering, electronics, and the Internet of Things. I also enjoy creating software tools and making basil pesto. </blockquote>\n\n")
-        file.write("> Most of the stuff on here is storage space.\n\n")
+        file.write("> Most of the stuff on here is storage space.\n")
+
+        # Writing the blog posts section
+        file.write("> [!NOTE]\n")
+        file.write("> Last personal updates:\n")
+        for post in posts:
+            file.write(f"  - [{post['title']}]({post['link']})\n")
 
         # Writing the last song listened section
         if song:
@@ -53,13 +74,8 @@ def update_readme(posts, song):
             file.write(f"| ![Cover Image]({song['image']}) | [{song['name']} by {song['artist']} - {song['album']}]({song['url']}) |\n")
             file.write("|---------------|:---------------------------------------------|\n\n")
 
-        # Writing the blog posts section
-        file.write("> [!NOTE]\n")
-        file.write("> Last personal updates:\n")
-        for post in posts:
-            file.write(f"  - [{post['title']}]({post['link']})\n")
-
 # Main execution
 posts = get_last_posts()
 song = get_last_song()
+bio = get_current_bio()
 update_readme(posts, song)
