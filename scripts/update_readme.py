d453213d36b1dd0276f7020edc0b92aa6290cf4d
Ezequiel Santos <ezequiel.ifsp@gmail.com>
2024-04-30 00:17:44 +0100

    Update update_readme.py

--- scripts/update_readme.py
+++ scripts/update_readme.py
@@ -3,26 +3,6 @@ import requests
 import datetime
 import os
 
-# > [!TIP]
-# > - 👋 **Hello!** Wishing you a wonderful Monday on this 29 of April of 2024.
-# > - 🙋🏻‍♂️ I'm **Ezequiel** (Ezekiel), a passionate developer and creative technologist.
-# > - 💼 Currently, I'm a **Mobile Developer** at [Miniclip](https://www.miniclip.com).
-# > - 🎓 I'm also pursuing a **PhD** in Digital Games Development at [IADE](https://www.iade.pt/en).
-# > - ⚡ Feel free to connect with me on [LinkedIn](https://www.linkedin.com/in/ezefranca).
-# >> Most of the stuff on here is storage space.
-
-# > [!NOTE]
-# > Last personal updates:
-#   > - [📃 Presenting at MobileSoft'24](https://ezefranca.com/news/presenting-mobilesoft-2024)
-#   > - [🎖️ Internal Hackathon Miniclip 2024](https://ezefranca.com/news/hackathon-miniclip-2024)
-#   > - [🥈 Tech for Good IADE 2024](https://ezefranca.com/news/tech-for-good-iade-2024)
-    
-# > [!IMPORTANT]
-# > > Last song listened.
-# > > | ![Cover Image](https://lastfm.freetls.fastly.net/i/u/64s/7b11b155153b825a3dbd6e4a600345ac.jpg) | [Mary Jane by Alanis Morissette - Jagged Little Pill (2015 Remaster)](https://www.last.fm/music/Alanis+Morissette/_/Mary+Jane) |
-# > >|---------------|:---------------------------------------------|
-
-
 # Obtain API keys from environment variables
 LASTFM_API_KEY = os.getenv('LASTFM_API_KEY')
 
@@ -85,13 +65,14 @@ def update_readme(posts, song, bio):
         file.write("> Last personal updates:\n")
         for post in posts:
             file.write(f">  - [{post['title']}]({post['link']})\n")
-
+        file.write("\n")
+        
         # Writing the last song listened section
         if song:
             file.write("> [!IMPORTANT]\n")
             file.write("> > Last song listened.\n\n")
-            file.write(f"> > | ![Cover Image]({song['image']}) | [{song['name']} by {song['artist']} - {song['album']}]({song['url']}) |\n")
-            file.write("> > |---------------|:---------------------------------------------|\n\n")
+            file.write(f"> > | ![Cover Image]({song['image']}) | [{song['name']}]({song['url']}) | {song['artist']} | {song['album']} |\n")
+            file.write("> >  |---------------|:---------------|:--------------|:------------|\n\n")
 
 # Main execution
 posts = get_last_posts()
