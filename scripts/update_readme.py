fa9f46098fcda4a053c671e1d34de5d3b306e241
Ezequiel Santos <ezequiel.ifsp@gmail.com>
2024-05-20 10:03:34 +0100

    Update update_readme.py

--- scripts/update_readme.py
+++ scripts/update_readme.py
@@ -395,7 +395,7 @@ def get_last_game_ns():
     last_issue = issues[-1]
     text_file_link = last_issue['body']
     response = requests.get(text_file_link)
-
+    print(text_file_link)
     if response.status_code != 200:
         print(f"Failed to fetch text file. Status code: {response.status_code}")
         return
